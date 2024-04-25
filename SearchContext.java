85dc0340553fb789a2405171675528e653c8bf51
Yocheved <yocheved@davidowitz.net>
2024-04-25 10:26:02 +0300

    context

--- SearchContext.java
+++ SearchContext.java
@@ -3,7 +3,6 @@ import java.util.List;
 
 public class SearchContext {
     private SearchFlightStrategy strategy;
-    private List<Flight> flights;
 
     public void setStrategy(SearchFlightStrategy s){
         this.strategy = s;
